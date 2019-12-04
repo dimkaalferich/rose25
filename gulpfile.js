15be1347748aa0cad5d0285b4776b12a4b9c8049
R.Volvaka <volvaka@d-partners.ru>
2019-12-04 16:37:27 +0300

    half of work

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+const gulp          = require('gulp');
+const gulpIf        = require('gulp-if');
+const browserSync   = require('browser-sync').create();
+const sass          = require('gulp-sass');
+const htmlmin       = require('gulp-htmlmin');
+const cssmin        = require('gulp-cssmin');
+const uglify        = require('gulp-uglify');
+const imagemin      = require('gulp-imagemin');
+const concat        = require('gulp-concat');
+const jsImport      = require('gulp-js-import');
+const sourcemaps    = require('gulp-sourcemaps');
+const htmlPartial   = require('gulp-html-partial');
+
+const isProd = process.env.NODE_ENV === 'prod';
+
+/*const htmlFile = [
+    'src/index.html',
+    'src/test.html'
+]*/
+
+const htmlFile = [
+    'src/index.html',
+    'src/catalog.html',
+    'src/contacts.html',
+    'src/basket.html'
+
+]
+
+function html() {
+    return gulp.src(htmlFile)
+        .pipe(htmlPartial({ basePath: 'src/partials/' }))
+        .pipe(gulpIf(isProd, htmlmin({ collapseWhitespace: true })))
+        .pipe(gulp.dest('dist'));  
+}
+
+function css() {
+    return gulp.src('src/sass/style.scss')
+        .pipe(gulpIf(!isProd, sourcemaps.init()))
+        .pipe(sass({ includePaths: ['node_modules'] }).on('error', sass.logError))
+        .pipe(gulpIf(!isProd, sourcemaps.write()))
+        .pipe(gulpIf(isProd, cssmin()))
+        .pipe(gulp.dest('dist/css/'));
+}
+
+function js() {
+    return gulp.src('src/js/*.js')
+        .pipe(jsImport({hideConsole: true}))
+        .pipe(concat('all.js'))
+        .pipe(gulpIf(isProd, uglify()))
+        .pipe(gulp.dest('dist/js'));
+}
+
+function img() {
+    return gulp.src('src/assets/img/*')
+        .pipe(gulpIf(isProd, imagemin()))
+        .pipe(gulp.dest('dist/assets/img/'));
+}
+
+function serve() {
+    browserSync.init({
+        open: true,
+        server: './dist'
+    });
+}
+
+function browserSyncReload(done) {
+    browserSync.reload();
+    done();
+}
+
+
+function watchFiles() {
+    gulp.watch('src/**/*.html', gulp.series(html, browserSyncReload));
+    gulp.watch('src/**/*.scss', gulp.series(css, browserSyncReload));
+    gulp.watch('src/**/*.js', gulp.series(js, browserSyncReload));
+    gulp.watch('src/assets/img/**/*.*', gulp.series(img));
+
+    return;
+}
+
+exports.css = css;
+exports.html = html;
+exports.js = js;
+exports.serve = gulp.parallel(html, css, js, img, watchFiles, serve);
+exports.default = gulp.parallel(html, css, js, img);
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+$(".to-close").on("click", () => {
+    $(".special-offer").hide();
+})
+
+$(".basket-product__close").on("click", () => {
+    $(".my-underline-row").hide();
+})
